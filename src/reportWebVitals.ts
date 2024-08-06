b583c1272516cbef1dfaed8a72f20a62eb91ed41
TommDaniel <daninho.tomm@gmail.com>
2024-08-05 23:11:56 -0300

    modify extension from files

---
+++ src/reportWebVitals.ts
@@ -0,0 +1,11 @@
+import { onCLS, onFCP, onLCP } from 'web-vitals';
+
+const reportWebVitals = (onPerfEntry?: (metric: any) => void) => {
+  if (onPerfEntry && onPerfEntry instanceof Function) {
+    onCLS(onPerfEntry);
+    onFCP(onPerfEntry);
+    onLCP(onPerfEntry);
+  }
+};
+
+export default reportWebVitals;
